d0291f1bad9bfd6697dfba28384c4c975e2cc03a
intteliHome <iseoulyou@gmail.com>
2023-05-11 19:58:14 +0900

    reply modify update done

--- src/main/java/org/zerock/board/controller/ReplyController.java
+++ src/main/java/org/zerock/board/controller/ReplyController.java
@@ -48,4 +48,14 @@ public class ReplyController {
         return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO){
+
+        log.info(replyDTO);
+
+        replyService.modify(replyDTO);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
 }
